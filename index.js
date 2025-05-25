5eb02cff5203931e19bfcc995f2ef642aa83fb13
datnpq2994 <dat.npq2994@gmail.com>
2025-05-25 15:27:38 +0700

    ua la sao

--- index.js
+++ index.js
@@ -4,35 +4,42 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const html = `
-    <!DOCTYPE html>
-    <html lang="vi">
-      <head>
-        <meta charset="UTF-8" />
-        <title>Đang chuyển hướng...</title>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4J7LFH0XTC"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+     <!DOCTYPE html>
+  <html lang="vi">
+    <head>
+      <meta charset="UTF-8" />
+      <title>Đang chuyển hướng...</title>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-4J7LFH0XTC"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-          gtag('config', 'G-4J7LFH0XTC', {
-            page_path: window.location.pathname + window.location.search
-          });
+        gtag('config', 'G-4J7LFH0XTC', {
+          page_path: window.location.pathname + window.location.search
+        });
 
-          gtag('event', 'redirect_to_shopee', {
-            event_category: 'engagement',
-            event_label: window.location.href
-          });
+        // Gửi event trước khi redirect
+        gtag('event', 'redirect_to_shopee', {
+          event_category: 'engagement',
+          event_label: window.location.href,
+          event_callback: function () {
+            setTimeout(() => {
+              window.location.href = "https://s.shopee.vn/3LEXW9cvAH";
+            }, 300);
+          }
+        });
 
-          setTimeout(function () {
-            window.location.href = "https://s.shopee.vn/3LEXW9cvAH";
-          }, 1500);
-        </script>
-      </head>
-      <body>
-        <p>Đang chuyển sang Shopee... Vui lòng chờ trong giây lát.</p>
-      </body>
-    </html>
+        // Fallback nếu GA không callback
+        setTimeout(() => {
+          window.location.href = "https://s.shopee.vn/3LEXW9cvAH";
+        }, 2500);
+      </script>
+    </head>
+    <body>
+      <p>Đang chuyển sang Shopee... Vui lòng chờ trong giây lát.</p>
+    </body>
+  </html>
   `;
   res.send(html);
 });
